6bb1f3d6f454948f55ba5939e27500ec1ec5701d
Paola Perdomo <paolaperdomo079@gmail.com>
2024-11-19 17:19:28 +0100

    first commit

---
+++ src/main/java/com/example/ShoppinListBackend/ShoppinListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoppinListBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppinListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppinListBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShoppinListBackend/ShoppinListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoppinListBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppinListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
